0196d07aeccb71bb610e2b0a293e581df5ee6294
Edunardi08 <edunardi1208@gmail.com>
2025-10-02 15:43:45 -0300

    Add files via upload

---
+++ Lista-4/ex-01.js
@@ -0,0 +1,9 @@
+let num1 = input.questionInt("Digite um número: ")
+let num2 = input.questionInt("Digite um número: ")
+
+if (num1 < num2) {
+    console.log("O menor número é: " + num1);
+}
+else {
+    console.log("O menor número é: " + num2)
+}
