c6f786dfe3a607429adbe8db9dac8049f1d332f3
Edunardi08 <edunardi1208@gmail.com>
2025-12-10 18:42:56 -0300

    Create exercicio-3.js

---
+++ lista-5/exercicio-3.js
@@ -0,0 +1,11 @@
+ let input = require('readline-sync')
+  let soma = 0;
+
+    // Laço para pedir 5 números
+    for (let i = 1; i <= 5; i++) {
+      let numero = input.questionInt("Digite o número ", i , ":");
+      soma += numero;
+    }
+
+    // Exibe a soma total
+    console.log("A soma total dos 5 números é: ", soma);
